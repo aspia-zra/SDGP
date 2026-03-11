4e9bcc4463abe3b8cdfed6391555e680b911bad5
Magicst4r <magicalmay23@gmail.com>
2026-03-11 22:21:56 +0000

    Refactor SQL queries to filter by locationID

--- models/admindashBE.py
+++ models/admindashBE.py
@@ -1,28 +1,41 @@
 # admindashBE.py
 from . import dbfunc
+from . import user_session
 import time, datetime
 
 def progressbarCalc():
     conn = dbfunc.getconnection()
     cursor = conn.cursor()
 
-    cursor.execute("SELECT COUNT(*) FROM Apartment")
+    cursor.execute("SELECT COUNT(*) FROM Apartment WHERE locationID=%s"
+        ,(user_session.user_base,))
     totalApt = cursor.fetchone()[0]
-    cursor.execute("SELECT COUNT(*) FROM Apartment WHERE Status='occupied'")
-    aptOccupied = cursor.fetchone()[0]
-    aptPercent = (aptOccupied/totalApt)*100
+    cursor.execute("SELECT COUNT(*) FROM Apartment WHERE Status='occupied' " \
+        "AND locationID=%s", (user_session.user_base,))
+    aptOccupied = cursor.fetchone()[0] 
+    aptPercent = (aptOccupied/totalApt)*100 if totalApt else 0
 
-    cursor.execute("SELECT COUNT(*) FROM Invoice")
-    totalInvoice = cursor.fetchone()[0]
-    cursor.execute("SELECT COUNT(*) FROM Invoice WHERE Status='paid'")
+    cursor.execute("""SELECT COUNT(*) FROM Invoice i
+        JOIN LeaseAgreement l ON i.leaseID = l.leaseID
+        JOIN Apartment a ON l.apartmentID = a.apartmentID
+        WHERE a.locationID=%s""" ,(user_session.user_base,))
+    totalInvoice = cursor.fetchone()[0] 
+    cursor.execute("""SELECT COUNT(*) FROM Invoice i
+        JOIN LeaseAgreement l ON i.leaseID = l.leaseID
+        JOIN Apartment a ON l.apartmentID = a.apartmentID
+        WHERE i.Status='paid' AND a.locationID=%s""", (user_session.user_base,))
     invoicePaid = cursor.fetchone()[0]
-    invoicePercent = (invoicePaid/totalInvoice)*100
+    invoicePercent = (invoicePaid/totalInvoice)*100 if totalInvoice else 0
 
-    cursor.execute("SELECT COUNT(*) FROM Complaint")
+    cursor.execute("""SELECT COUNT(*) FROM Complaint c
+        JOIN Apartment a ON c.apartmentID = a.apartmentID
+        WHERE a.locationID=%s""" ,(user_session.user_base,))
     totalComp = cursor.fetchone()[0]
-    cursor.execute("SELECT COUNT(*) FROM Complaint WHERE Status='closed'")
+    cursor.execute("""SELECT COUNT(*) FROM Complaint c
+        JOIN Apartment a ON c.apartmentID = a.apartmentID
+        WHERE c.Status='closed' AND a.locationID=%s""", (user_session.user_base,))
     compResolved = cursor.fetchone()[0]
-    compPercent = (compResolved/totalComp)*100
+    compPercent = (compResolved/totalComp)*100 if totalComp else 0
 
     cursor.close()
     conn.close()
@@ -35,10 +48,11 @@ def dropdownBoxes():
     cursor.execute("""SELECT l.endDate, u.fullName
         FROM LeaseAgreement l
         JOIN Tenant t ON l.tenantID = t.tenantID
-        Join UserTbl u ON t.userID = u.userID
-        WHERE l.endDate >= CURDATE()
+        JOIN UserTbl u ON t.userID = u.userID
+        JOIN Apartment a ON l.apartmentID = a.apartmentID
+        WHERE l.endDate >= CURDATE() AND a.locationID=%s
         ORDER BY l.endDate ASC
-        LIMIT 3""")
+        LIMIT 3""",(user_session.user_base, ))
     leases = cursor.fetchall()
 
     # Overdue rent
@@ -47,9 +61,11 @@ def dropdownBoxes():
         JOIN LeaseAgreement l ON i.leaseID = l.leaseID
         Join tenant t ON l.tenantID = t.tenantID
         Join UserTbl u ON t.userID = u.userID
-        WHERE i.dueDate <= CURDATE() AND i.Status = 'overdue'
+        JOIN Apartment a ON l.apartmentID = a.apartmentID
+        WHERE i.dueDate <= CURDATE() AND i.Status = 'overdue' 
+            AND a.locationID=%s
         ORDER BY i.dueDate ASC
-        LIMIT 3""")
+        LIMIT 3""",(user_session.user_base, ))
     overdues = cursor.fetchall()
 
     # High Priority Repairs
@@ -58,9 +74,10 @@ def dropdownBoxes():
         Join tenant t ON c.tenantID = t.tenantID
         Join UserTbl u ON t.userID = u.userID
         Join MaintenanceLog m ON c.complaintID = m.complaintID
-        WHERE c.Status = 'open'
+        JOIN Apartment a ON c.apartmentID = a.apartmentID           
+        WHERE c.Status = 'open' AND a.locationID=%s
         ORDER BY c.Severity DESC
-        LIMIT 3""")
+        LIMIT 3""",(user_session.user_base, ))
     repairs = cursor.fetchall()
 
     cursor.close()
@@ -77,11 +94,12 @@ def graph():
                    DATE_FORMAT(m.maintenanceDate, '%Y-%m') AS month 
             FROM MaintenanceLog m
             JOIN LeaseAgreement l ON m.apartmentID = l.apartmentID
+            JOIN Apartment a On l.apartmentID = a.apartmentID
             JOIN Invoice i ON l.leaseID = i.leaseID
-            WHERE i.Status = 'paid'
+            WHERE i.Status = 'paid' AND a.locationID = %s
             GROUP BY month
             ORDER BY month ASC
-            """)
+            """, (user_session.user_base,))
     profitloss = cursor.fetchall()
 
     graphs = []
@@ -89,3 +107,4 @@ def graph():
         graphs.append((item[1], item[0]))
 
     return graphs
+
