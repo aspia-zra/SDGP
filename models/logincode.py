c6d8f21f0459121676357d97c82ded8f40f951ff
drashtisamgi <drashti.samgi@gmail.com>
2026-03-24 17:24:30 +0000

    login code which i had to update
    Refactored UserTbl class to use get_connection and updated user_sessions handling.

--- models/logincode.py
+++ models/logincode.py
@@ -1,41 +1,46 @@
+from db.dbconnect import *
+import models.user_session as user_sessions
 import bcrypt
-from db.dbconnect import Database
-from models.usersession import user_sessions
 
 class UserTbl:
-    def __init__(self):
-        self.dbfunc = Database()
-        # Hashes a password using bcrypt before storing in the database
-        @staticmethod
-        def hash_password(password):
-            hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-            return hashed.decode()
 
-        @staticmethod
-        def login(email, password):
-            try:
-                conn = self.dbfunc.getconnection()
-                cursor = conn.cursor(dictionary=True)
+    @staticmethod
+    def hash_password(password):
+        hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+        return hashed.decode()
 
-                cursor.execute("SELECT * FROM UserTbl WHERE Email = %s", (email,))
-                user = cursor.fetchone()
-                conn.close()
+    @staticmethod
+    def login(email, password):
+        try:
+            conn = get_connection()
+            cursor = conn.cursor(dictionary=True)
 
-                if user is None:
-                    return None
+            cursor.execute("SELECT * FROM UserTbl WHERE Email = %s", (email,))
+            user = cursor.fetchone()
+            conn.close()
 
-                password_correct = bcrypt.checkpw(password.encode(), user["Password"].encode())
-
-                if password_correct:
-                    user_sessions.user_sessions(user)
-                    return user
-                else:
-                    return None
-
-            except Exception as e:
-                print("Login error:", e)
+            if user is None:
                 return None
-    
-        def logout():
-            user_sessions.logged_in = False
-            user_sessions.current_user_id = None
+
+            password_correct = bcrypt.checkpw(password.encode(), user["Password"].encode())
+
+            if password_correct:
+                user_sessions(user)
+                return user
+            else:
+                return None
+
+        except Exception as e:
+            print("Login error:", e)
+            return None
+
+    @staticmethod
+    def logout():
+        user_sessions.logged_in = False
+        user_sessions.current_user_id = None
+        user_sessions.current_user_name = ""
+        user_sessions.current_email = ""
+        user_sessions.current_phone = ""
+        user_sessions.user_type = ""
+        user_sessions.user_base = None
+        user_sessions.personal_fontsize = 12
