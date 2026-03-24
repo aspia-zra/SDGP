bae375445f7adde1780bd32faec565871d839d17
drashtisamgi <drashti.samgi@gmail.com>
2026-03-24 17:09:22 +0000

    removed database class so login works

--- db/dbconnect.py
+++ db/dbconnect.py
@@ -1,61 +1,57 @@
-from unittest import result
-
 import mysql.connector
 from mysql.connector import errorcode
 
-class Database:
-    def __init__(self):
-        self.hostname = "localhost"
-        self.db = "secondverparagonapartment"
-        self.username = "root"
-        self.passwd = "Drashtisamgi02!"
+hostname = "localhost"
+db = "secondverparagonapartment"
+username = "root"
+passwd = "Drashtisamgi02!"
 
-        self.conn = self.get_connection()
 
-    def get_connection(self):
-        try:
-            conn = mysql.connector.connect(
-                host=self.hostname,
-                user=self.username,
-                password=self.passwd,
-                database=self.db
-            )
-            return conn
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print('Username or password is incorrect')
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print('Database does not exist')
-            else:
-                print('Connection error:', err)
-            return None
-
-    def cursor(self):
-        if self.conn:
-            return self.conn.cursor()
+def get_connection():
+    try:
+        conn = mysql.connector.connect(
+                host=hostname,
+                user=username,
+                password=passwd,
+                database=db
+        )
+        return conn
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print('Username or password is incorrect')
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print('Database does not exist')
         else:
-            raise Exception("No database connection")
+            print('Connection error:', err)
+        return None
 
-    def commit(self):
-        if self.conn:
-            self.conn.commit()
-    
-    def execute(self, query, values=None):
-        cur = self.cursor()
-        cur.execute(query, values or ())
+def cursor():
+    conn = get_connection()
+    if conn:
+        return conn.cursor()
+    else:
+        raise Exception("No database connection")
+
+def commit(self):
+    if self.conn:
         self.conn.commit()
-        cur.close()
+    
+def execute(self, query, values=None):
+    cur = self.cursor()
+    cur.execute(query, values or ())
+    self.conn.commit()
+    cur.close()
 
-    def fetch_one(self, query, values=None):
-        cur = self.cursor()
-        cur.execute(query, values or ())
-        result = cur.fetchone()
-        cur.close()
-        return result
+def fetch_one(self, query, values=None):
+    cur = self.cursor()
+    cur.execute(query, values or ())
+    result = cur.fetchone()
+    cur.close()
+    return result
 
-    def fetch_all(self, query, values=None):
-        cur = self.cursor()
-        cur.execute(query, values or ())
-        result = cur.fetchall()
-        cur.close()
-        return result
+def fetch_all(self, query, values=None):
+    cur = self.cursor()
+    cur.execute(query, values or ())
+    result = cur.fetchall()
+    cur.close()
+    return result
