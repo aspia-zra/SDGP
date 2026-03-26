25bb21ce219da0a6a6591776c99d1505179b156e
Magicst4r <magicalmay23@gmail.com>
2026-03-26 12:17:56 +0000

    Enhance navbar for admin and manager modes
    Refactor navbar to support different user modes and update button functionalities.

---
+++ gui/Nav.py
@@ -0,0 +1,187 @@
+from tkinter import *
+import matplotlib
+import customtkinter as ctk
+
+from GUI import Admindash, adminreportView, settings, updatedfrontdesk
+from GUI.pages_mngdash import mngdashboard
+from Models import user_session
+from . import theme
+
+BG_COLOR = theme.BACKGROUND
+
+class navbar(ctk.CTkFrame):
+    def __init__(self, parent, controller, mode="manager"):
+        super().__init__(parent, fg_color=theme.SECONDARY)
+        self.controller = controller
+        self.mode = mode
+
+        self.grid(row=0, column=0, sticky="ns")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        if self.mode == "admin":
+            self.admin_nav()
+        elif self.mode == "manager":
+            self.mng_nav()
+        else:
+            self.mng_nav()
+        
+    # these are the different navbars - each function is a navbar we call for each page/dashboard
+     
+    def admin_nav(self):
+        self.navbar = ctk.CTkFrame(self, fg_color="transparent")
+        self.navbar.grid(row=0, column=0, sticky="ns")
+        self.navbar.grid_rowconfigure(7, weight=1)
+        self.navbar.grid_columnconfigure(0, weight=1)
+
+        #TITLE
+        navtitle_label = ctk.CTkLabel(
+            self.navbar, 
+            text="Paragon Apartments", 
+            fg_color= "transparent",
+            text_color=theme.PRIMARY,
+            font=(theme.TITLE_FONT, 20))
+        navtitle_label.grid(row = 0, column = 0, padx = 20, pady = (20,40), sticky="w")
+
+        btnConfig = { # Styling for buttons here
+            "fg_color": theme.PRIMARY,
+            "hover_color": theme.PRIMARY_DARK,
+            "text_color": theme.SURFACE,
+            "height": 40,
+            "corner_radius": 8,
+            "anchor": "w",
+        }
+
+        if user_session.user_type == "manager":
+            dashboard_text = "Apartment Management"
+            dashboard_command = self.open_mngdash
+        else:
+            dashboard_text = "Dashboard"
+            dashboard_command = self.open_admindash
+
+        dashboard = ctk.CTkButton(self.navbar,
+            command = dashboard_command,
+            text=dashboard_text, **btnConfig)
+        dashboard.grid(row = 1, column = 0, padx = 20, pady = 20, sticky = "ew")
+
+        reports = ctk.CTkButton(self.navbar,
+            command = self.open_adminreports, 
+            text="Reports View", **btnConfig)
+        reports.grid(row = 2, column = 0, padx = 20, pady = 20, sticky = "ew")
+
+        maintenance = ctk.CTkButton(self.navbar, 
+            command = self.open_maintdash, 
+            text="Maintenance View", **btnConfig)
+        maintenance.grid(row = 3, column = 0, padx = 20, pady = 20, sticky = "ew")
+
+        finance = ctk.CTkButton(self.navbar, 
+            command = self.open_financedash, 
+            text="Finance View", **btnConfig)
+        finance.grid(row = 4, column = 0, padx = 20, pady = 20, sticky = "ew")
+
+        frontdesk = ctk.CTkButton(self.navbar,
+            command = self.open_frontdash,  
+            text="Front-desk View", **btnConfig)
+        frontdesk.grid(row = 5, column = 0, padx = 20, pady = 20, sticky = "ew")
+
+        settings = ctk.CTkButton(self.navbar, 
+            command = self.open_settings,
+            text="Settings", **btnConfig)
+        settings.grid(row = 6, column = 0, padx = 20, pady = 20, sticky = "ew")
+
+        logout = ctk.CTkButton(self.navbar, 
+            fg_color=theme.PRIMARY, 
+            hover_color=theme.DANGER, 
+            text="Logout", 
+            command = self.logoutbtn)
+        logout.grid(row = 8, column = 0, padx = 20, pady = 20, sticky = "s")
+    
+    def mng_nav(self):
+        self.navbar = ctk.CTkFrame(self, fg_color="transparent")
+        self.navbar.grid(row=0, column=0, sticky="ns")
+        self.navbar.grid_rowconfigure(7, weight=1)
+        self.navbar.grid_columnconfigure(0, weight=1)
+
+        #TITLE
+        navtitle_label = ctk.CTkLabel(
+            self.navbar, 
+            text="Paragon Apartments", 
+            fg_color= "transparent",
+            text_color=theme.PRIMARY,
+            font=(theme.TITLE_FONT, 20))
+        navtitle_label.grid(row = 0, column = 0, padx = 20, pady = (20,40), sticky="w")
+
+        btnConfig = { # Styling for buttons here
+            "fg_color": theme.PRIMARY,
+            "hover_color": theme.PRIMARY_DARK,
+            "text_color": theme.SURFACE,
+            "height": 40,
+            "corner_radius": 8,
+            "anchor": "w",
+        }
+
+        dashboard = ctk.CTkButton(self.navbar,
+            command = self.open_admindash,
+            text="Admin View", **btnConfig)
+        dashboard.grid(row = 1, column = 0, padx = 20, pady = 20, sticky = "ew")
+
+        reports = ctk.CTkButton(self.navbar, 
+            text="Reports View", **btnConfig)
+        reports.grid(row = 2, column = 0, padx = 20, pady = 20, sticky = "ew")
+
+        maintenance = ctk.CTkButton(self.navbar, 
+            text="Maintenance View", **btnConfig)
+        maintenance.grid(row = 6, column = 0, padx = 20, pady = 20, sticky = "ew")
+        
+        settings = ctk.CTkButton(self.navbar, 
+            command = self.open_settings,
+            text="Settings", **btnConfig)
+        settings.grid(row = 3, column = 0, padx = 20, pady = 20, sticky = "ew")
+
+        logout = ctk.CTkButton(self.navbar, 
+            fg_color=theme.PRIMARY, 
+            hover_color=theme.DANGER, 
+            text="Logout", 
+            command = self.logoutbtn)
+        logout.grid(row = 8, column = 0, padx = 20, pady = 20, sticky = "s")    
+
+    # these are definitions for the buttons. only these buttons work for now
+    
+    def open_mngdash(self): 
+        self.controller.clear_page()
+        self.controller.current_page = mngdashboard(self.controller)
+        self.controller.current_page.grid(row=0, column=0, sticky="nsew")
+
+    def open_admindash(self): 
+        self.controller.clear_page()
+        self.controller.current_page = Admindash.admindashboard(self.controller)
+        self.controller.current_page.grid(row=0, column=0, sticky="nsew")
+
+    def open_adminreports(self): 
+        self.controller.clear_page()
+        self.controller.current_page = adminreportView.ReportsView(self.controller, self.controller)
+        self.controller.current_page.grid(row=0, column=0, sticky="nsew")
+
+    def open_maintdash(self):
+        self.controller.clear_page()
+        self.controller.current_page = settings.settings(self.controller)
+        self.controller.current_page.grid(row=0, column=0, sticky="nsew")
+
+    def open_financedash(self): 
+        self.controller.clear_page()
+        self.controller.current_page = Admindash.admindashboard(self.controller)
+        self.controller.current_page.grid(row=0, column=0, sticky="nsew")
+
+    def open_frontdash(self):
+        self.controller.clear_page()
+        self.controller.current_page = updatedfrontdesk.FrontDeskGUI(self.controller, self.controller)
+        self.controller.current_page.grid(row=0, column=0, sticky="nsew")
+
+    def open_settings(self): 
+        self.controller.clear_page()
+        self.controller.current_page = settings.settings(self.controller)
+        self.controller.current_page.grid(row=0, column=0, sticky="nsew")
+    
+    def logoutbtn(self):
+        # UserTbl.logout()
+        self.controller.show_login()
