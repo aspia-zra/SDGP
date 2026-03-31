476ecf56c0b466faa086190fba8fddf3299e176a
khanh-pham05 <2005khanhpham2005@gmail.com>
2026-03-31 15:05:12 +0100

    receipts save in utils folder

--- utils/receipt_generator.py
+++ utils/receipt_generator.py
@@ -1,8 +1,38 @@
+#from datetime import datetime
+
+#def generate_receipt(invoice_id, tenant_name, amount, due_date, status):
+    #filename = f"receipt_{invoice_id}.txt"
+    #content = f"""{'='*40}
+   #PARAGON APARTMENT MANAGEMENT
+         #PAYMENT RECEIPT
+#{'='*40}
+#Receipt Date : {datetime.now().strftime('%Y-%m-%d %H:%M')}
+#Invoice ID   : {invoice_id}
+#Tenant Name  : {tenant_name}
+#Amount       : {amount}
+#Due Date     : {due_date}
+#Status       : {status}
+#{'='*40}
+#Thank you for your payment.
+#"""
+   # with open(filename, "w") as f:
+       # f.write(content)
+   # return filename, content
+
+import os
 from datetime import datetime
 
 
 def generate_receipt(invoice_id, tenant_name, amount, due_date, status):
+
+    base_dir = os.path.dirname(__file__) 
+    receipts_dir = os.path.join(base_dir, "receipts")
+
+    os.makedirs(receipts_dir, exist_ok=True)
+
     filename = f"receipt_{invoice_id}.txt"
+    file_path = os.path.join(receipts_dir, filename)
+
     content = f"""{'='*40}
    PARAGON APARTMENT MANAGEMENT
          PAYMENT RECEIPT
@@ -16,6 +46,8 @@ Status       : {status}
 {'='*40}
 Thank you for your payment.
 """
-    with open(filename, "w") as f:
+
+    with open(file_path, "w") as f:
         f.write(content)
-    return filename, content
+
+    return file_path, content
